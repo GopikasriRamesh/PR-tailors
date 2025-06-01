8d539b841a4f2ef66f749a3f55809526ecd363e5
GopikasriRamesh <srigopika270@gmail.com>
2025-06-01 11:20:06 +0530

    service page created

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,11 +15,11 @@ const About = () => {
     <section className='py-10 px-4'>
        <div className='max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-10'>
         <div className='w-full md:w-1/2'>
-        <img src={ab1} alt="img1" className='rounded-2xl shadow-lg w-full object-cover'/>
+        <img src={ab1} alt="img1" className='rounded-3xl shadow-lg w-full object-cover'/>
         </div>
-        <div className='w-full md:w-1/2'>
-        <h3 className='text-2xl font-bold mb-4 text-black font-bold'>Personal Fittings That Understand You</h3>
-        <p text-gray-700 leading-relaxed>At PR TAILOR,it's not just tailoring-it's a one-on-one style journey tailored around your shape.Your needs and Your preferences.</p>
+        <div className='w-full md:w-1/2 border-2 rounded-3xl shadow-lg p-25 bg-gradient-to-br from-purple-300 to white-200'>
+        <h3 className='text-2xl font-bold mb-4 text-black font-bold '>Personal Fittings That Understand You</h3>
+        <p className='text-gray-700 leading-relaxed'>At PR TAILOR,it's not just tailoring-it's a one-on-one style journey tailored around your shape.Your needs and Your preferences.</p>
         </div>
        </div>
     </section>
@@ -27,12 +27,12 @@ const About = () => {
       <section className='py-10 px-4'>
        <div className='max-w-6xl mx-auto flex flex-col md:flex-row-reverse items-center gap-10'>
         <div className='w-full md:w-1/2'>
-        <img src={ab2} alt="img2" className='rounded-2xl shadow-lg w-full object-cover'/>
+        <img src={ab2} alt="img2" className='rounded-3xl shadow-lg w-full object-cover'/>
         </div>
 
-        <div className='w-full md:w-1/2 '>
+        <div className='w-full md:w-1/2 border-2 rounded-3xl shadow-lg p-23 bg-gradient-to-bl from-purple-300 to white-200'>
         <h3 className='text-2xl font-bold mb-4 text-black font-bold'>Every Outfit is Made to Empower</h3>
-        <p text-gray-700 leading-relaxed>We understand that every woman deserves to fell confident in what she wears-that's why we focus on creating pieces that not only fit the body but lift thye spirit.</p>
+        <p className='text-gray-700 leading-relaxed'>We understand that every woman deserves to fell confident in what she wears-that's why we focus on creating pieces that not only fit the body but lift thye spirit.</p>
         </div>
        </div>
     </section>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from 'react';
+import ser1 from '../assets/ser 1.png';
+import ser2 from '../assets/ser 2.png';
+import ser3 from '../assets/ser 3.png';
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <>
+    <section className="py-12 px-4 text-center">
+        <h2 className="text-2xl md:text-6xl font-semibold text-black mb-2">
+          Servies
+        </h2>
+    </section>
+    
+    <section className="px-6 py-10">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-10">
+        <div className="w-full md:w-1/2 relative">
+          <img
+            src={ser1}
+            alt="Blouses"
+            className="rounded-tr-[80px] rounded-bl-[80px] h-auto w-full"
+          />
+          <div className="absolute top-4 left-4 bg-white px-6 py-3 rounded-tr-[40px] shadow-md text-center">
+            <h2 className="text-4xl text-purple-700 font-semibold">Chudidar</h2>
+            <p className="text-purple-400 text-lg">Stitching</p>
+          </div>
+        </div>
 
-export default Services
+        <div className="w-full md:w-1/2 border-2  shadow-lg p-8 bg-gradient-to-br from-purple-300 to-white p-24 rounded-tl-[80px] rounded-br-[80px]">
+          <h3 className="text-2xl font-bold mb-4 text-black">
+            From Simple cotton to elegant festival wear,We sticth chudidars that blend comfort with grace
+          </h3>
+          <p className="text-gray-700 leading-relaxed">
+            -tailored perfectly to your shape and style
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section className="px-6 py-10">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row-reverse items-center gap-10">
+        <div className="w-full md:w-1/2 relative">
+          <img
+            src={ser2}
+            alt="Blouses"
+            className="rounded-tr-[80px] rounded-bl-[80px] h-auto w-full"
+          />
+          <div className="absolute top-4 left-4 bg-white px-6 py-3 rounded-tr-[40px] shadow-md text-center">
+            <h2 className="text-4xl text-purple-700 font-semibold">Blouses</h2>
+            <p className="text-purple-400 text-lg">Normal & Lining</p>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2 border-2  shadow-lg  bg-gradient-to-br from-purple-300 to-white p-24 rounded-tl-[80px] rounded-br-[80px]">
+          <h3 className="text-2xl font-bold mb-4 text-black">
+            Daily-Wear or Occasion-ready,every blouse is tailored for the perfect fit and elegant finish
+          </h3>
+          <p className="text-gray-700 leading-relaxed">
+             -always matching your saree's style
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section className="px-6 py-10">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-10">
+        <div className="w-full md:w-1/2 relative ">
+         <img
+  src={ser3}
+  alt="Blouses"
+  className="rounded-tr-[80px] rounded-bl-[80px] h-[420px] w-full object-cover"
+/>
+
+          <div className="absolute top-4 left-4 bg-white px-6 py-3 rounded-tr-[40px] shadow-md text-center">
+            <h2 className="text-4xl text-purple-700 font-semibold">Embroidery</h2>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2 border-2  shadow-lg  bg-gradient-to-br from-purple-300 to-white p-23 rounded-tl-[80px] rounded-br-[80px]">
+          <h3 className="text-2xl font-bold mb-4 text-black">
+            Add a touch of elegance with our intricate embroidery work-perfect for blouses,dupattas.sleeves and more
+          </h3>
+          <p className="text-gray-700 leading-relaxed">
+            -every detail is crafted to make your outfit truely unique
+          </p>
+        </div>
+      </div>
+    </section>
+    </>
+  );
+};
+
+export default Services;
